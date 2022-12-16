c18fcf47d77fadcb16f8bc8eaa20587e1fc74d74
gyansahoo666 <119865696+gyansahoo666@users.noreply.github.com>
2022-12-16 05:50:39 -0800

    assertion updated

--- samples/automation-test/automation-test-script.js
+++ samples/automation-test/automation-test-script.js
@@ -123,7 +123,8 @@ describe('Android App sample', () => {
   it('should show the app label', async () => {
     await driver.elementByClassName("android.widget.TextView")
       .text().then(function (text) {
-        assert.equal(text.toLocaleLowerCase(), 'api demos')
+        assert.equal(text.toLocaleLowerCase(), 'choose what to allow api demos to access')
+      
       })
   })
 
