00e471a2a807ce791c9ea956f9a9edb7c6fa0109
gyansahoo666 <119865696+gyansahoo666@users.noreply.github.com>
2022-12-16 05:11:38 -0800

    desiredCaps changed

--- samples/automation-test/automation-test-script.js
+++ samples/automation-test/automation-test-script.js
@@ -22,16 +22,53 @@ const kobitonServerConfig = {
   auth: `${username}:${apiKey}`
 }
 
-const desiredCaps = {
-  sessionName: 'Automation Test Session',
-  sessionDescription: 'This is an example of automation testing of an Android application',
-  deviceOrientation: deviceOrientation,
-  captureScreenshots: captureScreenshots,
-  deviceName: deviceName,
-  deviceGroup: deviceGroup,
-  platformName: platformName,
-  app: app
+// const desiredCaps = {
+//   sessionName: 'Automation Test Session',
+//   sessionDescription: 'This is an example of automation testing of an Android application',
+//   deviceOrientation: deviceOrientation,
+//   captureScreenshots: captureScreenshots,
+//   deviceName: deviceName,
+//   deviceGroup: deviceGroup,
+//   platformName: platformName,
+//   app: app
+// }
+
+
+var webdriverKobitonServerConfig = {
+  protocol: 'https',
+  //host: 'api.kobiton.com',
+  //auth: 'gyanadeeps:15a9ea3f-38fb-450c-9706-08a72ed71950'
 }
+
+var desiredCaps = {
+  sessionName:        'Automation test session',
+  sessionDescription: '',
+  deviceOrientation:  'portrait',
+  noReset:            true,
+  fullReset:          false,
+  captureScreenshots: true,
+  // The maximum size of application is 4000MB
+  // By default, HTTP requests from testing library are expired
+  // in 2 minutes while the app copying and installation may
+  // take up-to 30 minutes. Therefore, you need to extend the HTTP
+  // request timeout duration in your testing library so that
+  // it doesn't interrupt while the device is being initialized.
+  app:                'https://appium.github.io/appium/assets/ApiDemos-debug.apk',
+  
+  // The given team is used for finding devices and the created session will be visible for all members within the team.
+  groupId:            12170, // Group: AutomationDocs
+  deviceGroup:        'ORGANIZATION',
+  // For deviceName, platformVersion Kobiton supports wildcard
+  // character *, with 3 formats: *text, text* and *text*
+  // If there is no *, Kobiton will match the exact text provided
+  deviceName:         'Galaxy S20+ 5G (Payload Enabled)',
+  // The tag is used for finding devices and the user can input only one tag. 
+  // For example, the data value will be inputted: tagName="TagName1"
+  tagName:            '',
+  platformVersion:    '13',
+  platformName:       'Android'
+} 
+
 
 let driver
 
@@ -86,3 +123,12 @@ describe('Android App sample', () => {
     }
   })
 })
+
+// //wd.configureHttp({
+// 	timeout: 20 * 60000, // 20 mins
+// 	retries: 3,
+// 	retryDelay: 1000
+// })
+
+// const driver = wd.promiseChainRemote(serverConfig)
+// await driver.init(capabilities)
